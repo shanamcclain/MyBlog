0ee843d8d0700c1e2de1e73a03a12d0e7ac802de
shanamcclain <35242747+shanamcclain@users.noreply.github.com>
2018-02-27 10:18:13 -0500

    update

--- Models/BlogPost.cs
+++ Models/BlogPost.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MyBlog.Models
 {
@@ -14,10 +15,11 @@ namespace MyBlog.Models
         public DateTimeOffset Created { get; set; }
         public DateTimeOffset? Updated { get; set; }
         public string Title { get; set; }
-        public string Slug { get; set; }
+       [AllowHtml]
         public string Body { get; set; }
         public string MediaUrl { get; set; }
         public bool Published { get; set; }
+        public string Slug { get; set; }
 
         public virtual ICollection<Comment> Comments { get; set; }
     }
